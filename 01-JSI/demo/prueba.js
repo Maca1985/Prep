64b601f9e389b403dcea211e44d5c5a41002f289
Maca Calvo <macucalvo@gmail.com>
2021-11-04 01:02:36 -0300

    JSI 1

---
+++ 01-JSI/demo/prueba.js
@@ -0,0 +1,12 @@
+// for (let index = 1; index <= 10; index = index + 2) {
+//     console.log(index);
+// }
+
+// console.log('Termine!')
+
+let num = 1
+while(num <= 10) {
+    console.log(num); 
+    num++;
+}
+console.log('Termine!')
